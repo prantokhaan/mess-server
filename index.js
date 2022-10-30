437077d2e62f9a636d645a62a3eff727b48cca16
Ismatul Islam Pranto <prantokhaan@gmail.com>
2022-10-31 03:34:03 +0600

    fixed to previous

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/users/", require("./routes/usersRoute"));
 app.use("/deposit/", require("./routes/depositRoute"));
@@ -16,23 +15,7 @@ app.use("/other/", require("./routes/otherRoute"));
 app.use("/bazar/", require("./routes/bazarRoute"));
 app.use("/not/", require("./routes/notRoute"));
 
-app.put("/updateStatus/:id", async (req, res) => {
-  const id = req.params.id;
-  const newStatus = req.body;
-  const filter = { _id: id };
-  const options = { upsert: true };
-  const updateStatus = {
-    $set: {
-      status: newStatus[0],
-    },
-  };
-  const result = await notsCollection.updateOne(
-    filter,
-    updateStatus,
-    options
-  );
-  res.json(result);
-});
+
 
 
 
